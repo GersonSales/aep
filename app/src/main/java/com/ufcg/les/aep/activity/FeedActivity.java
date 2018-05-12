4c77b633423c709fd4d30b3b05468d37059a4197
joaolcaas <joao.felipe@ccc.ufcg.edu.br>
2018-05-12 00:24:12 -0300

    toolbar (pesquisa e cadastro)

--- app/src/main/java/com/ufcg/les/aep/activity/FeedActivity.java
+++ app/src/main/java/com/ufcg/les/aep/activity/FeedActivity.java
@@ -9,10 +9,27 @@
 package com.ufcg.les.aep.activity;
 
 import android.os.Bundle;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SyncStatusObserver;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.ufcg.les.aep.MainActivity;
 import com.ufcg.les.aep.R;
 import com.ufcg.les.aep.adapter.PostAdapter;
 
@@ -25,6 +42,13 @@ import butterknife.ButterKnife;
  * {@link com.ufcg.les.aep.model.post.Post}.
  */
 public class FeedActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private MenuItem mSearchAction;
+    private MenuItem mCadastraAction;
+    private boolean isSearchOpened = false;
+    private EditText edtSearch;
+
+
   
   /**
    * This RecyclerView is responsible to list the {@link com.ufcg.les.aep.model.post.Post} and
@@ -46,6 +70,8 @@ public class FeedActivity extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_feed);
     ButterKnife.bind(this);
+    mToolbar = findViewById(R.id.toolbar);
+    setSupportActionBar(mToolbar);
     
     initRecyclerView();
   }
@@ -57,4 +83,85 @@ public class FeedActivity extends AppCompatActivity {
     feedRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     feedRecyclerView.setAdapter(PostAdapter.getInstance());
   }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        mSearchAction = menu.findItem(R.id.action_search);
+        mCadastraAction = menu.findItem(R.id.action_cadastro);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.action_search:
+                handleMenuSearch();
+                return true;
+            case R.id.action_cadastro:
+
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected void handleMenuSearch(){
+        ActionBar action = getSupportActionBar(); //get the actionbar
+
+        if(isSearchOpened){ //test if the search is open
+
+            action.setDisplayShowCustomEnabled(false); //disable a custom view inside the actionbar
+            action.setDisplayShowTitleEnabled(true); //show the title in the action bar
+
+            //hides the keyboard
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(edtSearch.getWindowToken(), 0);
+
+            //add the search icon in the action bar
+            //mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_open_search)); onde mudar o icone
+
+            isSearchOpened = false;
+        } else { //open the search entry
+
+            action.setDisplayShowCustomEnabled(true); //enable it to display a
+            // custom view in the action bar.
+            action.setCustomView(R.layout.search_bar);//add the custom view
+            action.setDisplayShowTitleEnabled(false); //hide the title
+
+            edtSearch = action.getCustomView().findViewById(R.id.edtSearch); //the text editor
+
+            //this is a listener to do a search when the user clicks on search button
+            edtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                        //doSearch(); onde fazer a pesquisa
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+
+            edtSearch.requestFocus();
+
+            //open the keyboard focused in the edtSearch
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(edtSearch, InputMethodManager.SHOW_IMPLICIT);
+
+
+            //add the close icon
+            //mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_close_search));
+
+            isSearchOpened = true;
+        }
+    }
 }
