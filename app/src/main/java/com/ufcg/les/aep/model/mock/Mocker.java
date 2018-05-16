9aed76caa728558441157c0d228cdff2319c117d
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-05-16 00:13:37 -0300

    creating the swap to refresh behaviour'

--- app/src/main/java/com/ufcg/les/aep/model/mock/Mocker.java
+++ app/src/main/java/com/ufcg/les/aep/model/mock/Mocker.java
@@ -1,7 +1,14 @@
 package com.ufcg.les.aep.model.mock;
 
+import android.util.Log;
+
 import com.ufcg.les.aep.model.post.Post;
+import com.ufcg.les.aep.util.LogTag;
 
 public class Mocker {
     public static final Mock<Post> POST_MOCK = new Mock<>();
+  
+  public static void update() {
+    Log.i(LogTag.UPDATE, "Updating the Mocker.");
+  }
 }
