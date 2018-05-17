df5e2dca3510f95cfb7a6cf6468a3d9397b09990
Igor Brasileiro <brasileiro456@gmail.com>
2018-05-17 01:36:54 -0300

    add query functionality

--- app/src/main/java/com/ufcg/les/aep/activity/FeedActivity.java
+++ app/src/main/java/com/ufcg/les/aep/activity/FeedActivity.java
@@ -8,8 +8,10 @@
 
 package com.ufcg.les.aep.activity;
 
+import android.app.SearchManager;
 import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
@@ -17,10 +19,13 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.SearchView;
 
 import com.microsoft.appcenter.AppCenter;
 import com.microsoft.appcenter.analytics.Analytics;
@@ -28,7 +33,12 @@ import com.microsoft.appcenter.crashes.Crashes;
 import com.ufcg.les.aep.R;
 import com.ufcg.les.aep.adapter.PostAdapter;
 import com.ufcg.les.aep.model.mock.Mocker;
+import com.ufcg.les.aep.model.post.Post;
 import com.ufcg.les.aep.util.Constant;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,29 +50,30 @@ import static com.ufcg.les.aep.util.Constant.APP_SECRET;
  * This class is an {@link android.app.Activity} that provides the listing of the
  * {@link com.ufcg.les.aep.model.post.Post}.
  */
-public class FeedActivity extends AppCompatActivity {
+public class FeedActivity extends AppCompatActivity implements SearchView.OnQueryTextListener {
     private Toolbar mToolbar;
     private MenuItem mSearchAction;
     private boolean isSearchOpened = false;
     private EditText edtSearch;
+    private List<Post> backupMock;
 
 
-  
+
   /**
    * This RecyclerView is responsible to list the {@link com.ufcg.les.aep.model.post.Post} and
    * show then in the {@link FeedActivity} layout. It was bound by the {@link ButterKnife}.
    */
   @BindView(R.id.feed_recyclerView)
   RecyclerView feedRecyclerView;
-  
+
   /**
    * This field is the representation of the {@link SwipeRefreshLayout} that's responsible o create
    * a refresh interaction with the feed_recyclerView layout.
    */
   @BindView(R.id.feed_swipeRefresh)
   SwipeRefreshLayout feedRefresher;
-  
-  
+
+
   /**
    * This method is responsible to generate all {@link FeedActivity} behaviour when it's created
    * by the Android life cycle.
@@ -77,16 +88,17 @@ public class FeedActivity extends AppCompatActivity {
     ButterKnife.bind(this);
     mToolbar = findViewById(R.id.toolbar);
     setSupportActionBar(mToolbar);
-    
+
+
     initRecyclerView();
     initAppCenterAnalytics();
   }
-  
+
   private void initAppCenterAnalytics() {
     AppCenter.start(getApplication(), APP_SECRET,
        Analytics.class, Crashes.class);
   }
-  
+
   /**
    * This method ensures that the {@link FeedActivity}'s {@link RecyclerView} is create correctly.
    */
@@ -95,79 +107,75 @@ public class FeedActivity extends AppCompatActivity {
     feedRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     feedRecyclerView.setAdapter(PostAdapter.getInstance());
   }
-  
+
   private void initFeedRefresher() {
     this.feedRefresher.setOnRefreshListener(this :: refreshFeed);
   }
-  
+
   private void refreshFeed() {
     Mocker.update();
     this.feedRefresher.setRefreshing(false);
   }
-  
+
   @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main,menu);
+  public boolean onCreateOptionsMenu(Menu menu) {
+      MenuInflater inflater = getMenuInflater();
+      inflater.inflate(R.menu.menu_main, menu);
+
+      SearchManager searchManager =
+              (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+      SearchView searchView =
+              (SearchView) menu.findItem(R.id.search).getActionView();
+      searchView.setSearchableInfo(
+              searchManager.getSearchableInfo(getComponentName()));
+
+      searchView.setOnQueryTextListener(this);
+
+      return true;
+  }
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        copyToFrom(Mocker.POST_MOCK.getList(), backupMock);
+        backupMock = null;
+
         return true;
     }
 
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        mSearchAction = menu.findItem(R.id.action_search);
-        return super.onPrepareOptionsMenu(menu);
-    }
+    public boolean onQueryTextChange(String newText) {
+        if(!newText.isEmpty()) {
+            System.out.println("Porra Junio");
+            System.out.println("Porra Junio");
+            System.out.println("Porra Junio");
+            if(backupMock == null) {
+                backupMock = new ArrayList<>();
+                copyToFrom(backupMock, Mocker.POST_MOCK.getList());
+            }
 
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        switch (id){
-            case R.id.action_search:
-                handleMenuSearch();
-                return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    protected void handleMenuSearch(){
-        ActionBar action = getSupportActionBar();
-
-        if(isSearchOpened){
-            action.setDisplayShowCustomEnabled(false);
-            action.setDisplayShowTitleEnabled(true);
-            
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.hideSoftInputFromWindow(edtSearch.getWindowToken(), 0);
-
-            //mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_open_search)); onde mudar o icone TODO
-
-            isSearchOpened = false;
-        } else { //open the search entry
-
-            action.setDisplayShowCustomEnabled(true);
-            // custom view in the action bar.
-            action.setCustomView(R.layout.search_bar);
-            action.setDisplayShowTitleEnabled(false);
-
-            edtSearch = action.getCustomView().findViewById(R.id.edtSearch); //the text editor
-
-            edtSearch.setOnEditorActionListener((v, actionId, event) -> {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                    //doSearch(); onde fazer a pesquisa TODO
-                    return true;
+            List<Post> newMock = new ArrayList<>();
+            System.out.println(newText);
+            for(Post post: Mocker.POST_MOCK.getList()) {
+                if(post.getTitle().charAt(newText.length()-1) == (newText.charAt(newText.length()-1))) {
+                    newMock.add(post);
                 }
-                return false;
-            });
+            }
 
+            Mocker.POST_MOCK.setList(newMock);
+            return true;
 
-            edtSearch.requestFocus();
+        } else {
+            copyToFrom(Mocker.POST_MOCK.getList(), backupMock);
+            backupMock = null;
+            System.out.println("Otaro nem entrei");
 
-            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-            imm.showSoftInput(edtSearch, InputMethodManager.SHOW_IMPLICIT);
+            return false;
+        }
+    }
 
-            //mSearchAction.setIcon(getResources().getDrawable(R.drawable.ic_close_search));TODO
-            isSearchOpened = true;
+    private void copyToFrom(List to, List<Post> from) {
+        for(Post p: from) {
+            to.add(p);
         }
     }
 }
