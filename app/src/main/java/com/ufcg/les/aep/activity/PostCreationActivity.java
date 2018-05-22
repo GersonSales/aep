23e79900d7d2ef4165df4726f94c2044707f400d
Igor Brasileiro <brasileiro456@gmail.com>
2018-05-22 19:55:34 -0300

    creating post creation layout

---
+++ app/src/main/java/com/ufcg/les/aep/activity/PostCreationActivity.java
@@ -0,0 +1,15 @@
+package com.ufcg.les.aep.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.ufcg.les.aep.R;
+
+public class PostCreationActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_creation);
+    }
+}
