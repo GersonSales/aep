f609a7e1caf13095abc61d9f4c06e2968001d2c9
GersonSales <gerson.junior@ccc.ufcg.edu.br>
2018-05-22 21:17:39 -0300

    adding fields to user

--- app/src/main/java/com/ufcg/les/aep/activity/PostCreationActivity.java
+++ app/src/main/java/com/ufcg/les/aep/activity/PostCreationActivity.java
@@ -2,14 +2,30 @@ package com.ufcg.les.aep.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
 
 import com.ufcg.les.aep.R;
 
-public class PostCreationActivity extends AppCompatActivity {
 
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class PostCreationActivity extends AppCompatActivity {
+  
+  
+  @BindView(R.id.dateEdit_editText)
+  EditText dateUser;
+  
+  
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post_creation);
+        ButterKnife.bind(this);
     }
+    
+    
+    
 }
